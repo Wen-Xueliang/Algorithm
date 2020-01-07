baabfd3cafbf77887d18ceac9acc0de914a7a5cd
Wen-Xueliang <lgwenxla@outlook.com>
2020-01-07 21:51:11 +0800

    add sort

---
+++ src/eng/lab/algorithm/sort/common/Sort.java
@@ -0,0 +1,10 @@
+package eng.lab.algorithm.sort.common;
+
+/**
+ * @Author: Wen-Xueliang
+ * @Date: Created in 2019/9/19 22:40
+ * @Description: 排序基类
+ */
+public interface Sort {
+    int[] sort(int[] list);
+}
